4f63ed6cb6f717f862c273d78cbc48468138020e
Rick Toews <rick.toews@gmail.com>
2019-08-30 16:55:37 -0700

    Some minor adjustments.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import us_map from './us_map.svg';
+import React from 'react';
 import usStates from './us-map-data.js';
 import './App.css';
 
 function App() {
   const style = {fill:"#d3d3d3",stroke:"#ffffff",strokeOpacity:1,strokeWidth:0.75,strokeMiterlimit:4,strokeDasharray:"none"};
 
-  useEffect(() => {
-    console.log('called useEffect', document.querySelectorAll('path'));
-  });
-
   const unHighlightState = e => {
     let target = e.target;
-    target.setAttribute("style", "fill: gray");
+    target.setAttribute("style", "fill: rgb(211, 211, 211)");
     e.stopPropagation();
   }
   
@@ -27,20 +21,15 @@ function App() {
   
   return (
     <div className="App">
-<svg
-   onMouseOver={highlightState}
-   onMouseOut={unHighlightState}
-   xmlns="http://www.w3.org/2000/svg"
-   width="959"
-   height="593"
-   id="us-map">
-{usStates.map(st => (
-  <path
-     style={style}
-     id={st.id}
-     d={st.d} />
-    ))}
-  </svg>
+      <svg
+         onMouseOver={highlightState}
+         onMouseOut={unHighlightState}
+         xmlns="http://www.w3.org/2000/svg"
+         width="959"
+         height="593"
+         id="us-map">
+         {usStates.map(st => <path style={style} id={st.id} d={st.d} />)}
+      </svg>
     </div>
   );
 }
